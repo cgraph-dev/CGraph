aec5d28895c2048d1b915fe5a55c9f36a379aec1
Lucas Burca <contactlucasburca@gmail.com>
2026-02-04 21:10:20 +0200

    refactor: extract searchscreen components

---
+++ apps/mobile/src/screens/search/SearchScreen/components/index.ts
@@ -0,0 +1,16 @@
+/**
+ * SearchScreen Components
+ *
+ * @module screens/search/SearchScreen/components
+ */
+
+export {
+  SkeletonLoader,
+  TrendingItem,
+  VoiceSearchButton,
+  AnimatedResultItem,
+  FilterModal,
+  defaultFilters,
+} from './SearchComponents';
+
+export type { SearchFilters } from './SearchComponents';
