69769af25c4bc5d407ca43af702a6d365807ddb2
Lucas Burca <contactlucasburca@gmail.com>
2026-03-10 01:32:40 +0200

    feat(28-01): build nameplate-bar component with lottie and css rendering

---
+++ apps/web/src/components/nameplate/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Nameplate components barrel export.
+ *
+ * @module components/nameplate
+ */
+
+export { NameplateBar } from './NameplateBar';
+export type { NameplateBarProps } from './NameplateBar';
